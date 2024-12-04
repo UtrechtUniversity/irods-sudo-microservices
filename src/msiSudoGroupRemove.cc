3bf4772d230735ef00a4e7b47fa8740c307e4bac
Lazlo Westerhof <l.r.westerhof@uu.nl>
2024-12-04 15:11:28 +0100

    Adjust msiSudoGroupRemove for the rsGeneralAdmin API change (irods/irods#7380)

--- src/msiSudoGroupRemove.cc
+++ src/msiSudoGroupRemove.cc
@@ -2,7 +2,7 @@
  * \file
  * \brief     Group remove sudo microservice.
  * \author    Chris Smeele
- * \copyright Copyright (c) 2016, 2017, Utrecht University
+ * \copyright Copyright (c) 2016, 2017, 2024, Utrecht University
  */
 #include "common.hh"
 #include <rsGeneralAdmin.hpp>
@@ -22,7 +22,7 @@ namespace Sudo {
         generalAdminInp_t adminParams = { };
 
         adminParams.arg0 = const_cast<char*>("rm");
-        adminParams.arg1 = const_cast<char*>("user");
+        adminParams.arg1 = const_cast<char*>("group"); // https://github.com/irods/irods/issues/7380
         adminParams.arg2 = const_cast<char*>(groupName.c_str());
         adminParams.arg3 = const_cast<char*>(rei->uoic->rodsZone);
         adminParams.arg4 = const_cast<char*>("");
